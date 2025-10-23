61541dfc1eb7302364fabf1c29b33c2acbfdba49
josiasdev <francisco.batista67@alu.ufc.br>
2025-10-23 09:12:55 -0300

    feat: Adicionando o botão do whatsapp.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,6 +7,7 @@ import { Projects } from "@/components/Projects";
 import { Education } from "@/components/Education";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
+import { WhatsAppButton } from "@/components/WhatsAppButton/WhatsAppButton";
 
 const Index = () => {
   return (
@@ -21,6 +22,7 @@ const Index = () => {
         <Education />
         <Contact />
       </main>
+      <WhatsAppButton />
       <Footer />
     </div>
   );
