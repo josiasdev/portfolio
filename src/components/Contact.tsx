30dcf063d6e8cff53c3bb94244f061a0c29c04db
josiasdev <francisco.batista67@alu.ufc.br>
2025-11-04 22:06:33 -0300

    feat: novo email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -33,8 +33,8 @@ const Contact = () => {
     {
       icon: Mail,
       label: t('contact.email'),
-      value: 'francisco.batista67@alu.ufc.br',
-      href: 'mailto:francisco.batista67@alu.ufc.br',
+      value: 'JBDevSoftware@proton.me',
+      href: 'mailto:JBDevSoftware@proton.me',
     },
     {
       icon: Phone,
